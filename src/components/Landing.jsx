76ae21a7750383b92e3a98d5acfb2919bb323c2c
carlos <carlosdummy14@gmail.com>
2022-03-16 21:55:52 -0600

    Add landing component

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,16 +1,43 @@
-const Landing = () => {
+import './Landing.css'
+import handRock from '../assets/fa-regular_hand-rock.svg'
+import handPaper from '../assets/fa-regular_hand-paper.svg'
+import handScissors from '../assets/fa-regular_hand-scissors.svg'
+
+const Landing = ({ setGameStatus }) => {
   return (
-    <div className="container">
-      <div className="left-panel">
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-      </div>
-      <button>NEW GAME</button>
-      <div className="right-panel">
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
+    <div className="landing">
+      <div className="wrapper">
+        <div className="img-column">
+          <img className="landing-image" src={handScissors} alt="scissors" />
+          <img className="landing-image" src={handPaper} alt="paper" />
+          <img className="landing-image" src={handRock} alt="rock" />
+        </div>
+        <div className="center">
+          <button
+            className="btn btn-large"
+            onClick={() => setGameStatus('play')}
+          >
+            NEW GAME
+          </button>
+          <p className="text">Roll 10 times and win</p>
+        </div>
+        <div className="img-column">
+          <img
+            className="landing-image image-right"
+            src={handPaper}
+            alt="paper"
+          />
+          <img
+            className="landing-image image-right"
+            src={handRock}
+            alt="rock"
+          />
+          <img
+            className="landing-image image-right"
+            src={handScissors}
+            alt="scissors"
+          />
+        </div>
       </div>
     </div>
   )
