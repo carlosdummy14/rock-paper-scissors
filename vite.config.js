3eeb6d0a69877f815bbee20726f70102b3787fc1
carlos <carlosdummy14@gmail.com>
2022-03-16 23:26:26 -0600

    Config gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const path = require('path')
+const mode =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development'
+const base =
+  mode === 'production' ? '/' + path.basename(process.cwd()) + '/' : '/'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: 'src',
+  base,
+  mode,
   build: {
     outDir: '../dist',
     assetsDir: './'
