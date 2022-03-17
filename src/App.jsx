5574ae0c00a0a23a1f4ad6c0ac4c25b98954c28e
carlos <carlosdummy14@gmail.com>
2022-03-16 21:57:06 -0600

    Put all together

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { useState } from 'react'
 import Landing from './components/Landing'
 import Main from './components/Main'
 
 function App() {
+  const [gameStatus, setGameStatus] = useState('start')
   return (
     <div className="container">
-      <Main />
+      {gameStatus === 'start' && <Landing setGameStatus={setGameStatus} />}
+      {gameStatus === 'play' && <Main setGameStatus={setGameStatus} />}
     </div>
   )
 }
